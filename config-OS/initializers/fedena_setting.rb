471d6c2c2c6f65912db18ba6db22c5498c24039e
mohammadbilalarif <billal.ariif@gmail.com>
2016-06-19 08:21:33 +0500

    Openshift Configuration

---
+++ config-OS/initializers/fedena_setting.rb
@@ -0,0 +1,15 @@
+FEDENA_DEFAULTS = {
+  :company_name => 'Processing Inc.',
+  :company_url  => 'www.processinginc.co'
+}
+
+USER_SETTINGS = {}
+
+if File.exists?("#{RAILS_ROOT}/config/company_details.yml")
+  company_settings = YAML.load_file(File.join(RAILS_ROOT,"config","company_details.yml"))
+  USER_SETTINGS = {:company_name => company_settings['company_details']['company_name'],
+                   :company_url  => company_settings['company_details']['company_url']
+  }
+end
+
+FEDENA_SETTINGS = FEDENA_DEFAULTS.merge!(USER_SETTINGS)
